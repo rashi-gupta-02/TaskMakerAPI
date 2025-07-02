e12a71d60e55df43470a31a79fde3072f7d82d9e
rashi-gupta-02 <rashi@equantra.in>
2025-07-02 18:43:41 +0530

    Add files via upload

---
+++ src/main/java/com/example/project/controller/Task.java
@@ -0,0 +1,23 @@
+package com.example.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+}
---
+++ src/main/java/com/example/project/controller/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.project.repository;
+import com.example.project.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCompleted(boolean completed);
+}
